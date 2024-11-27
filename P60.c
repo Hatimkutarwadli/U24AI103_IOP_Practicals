b4eedf2d5d5384fd575cf2eb443e5304a18ca039
Hatim Kutarwadli <62584115+Hatimkutarwadli@users.noreply.github.com>
2024-11-27 10:12:26 +0530

    Update P60.c

--- P60.c
+++ P60.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 int main () {
-    int min = 0;
-    int c;
-    int arr[] = {2,5,1,3,4};
-    int sortedarr[5];
-    for (int i=0; i<4 ; i++) {
-        min = arr[i];
-        for (int j=0; j<5; j++) {
-            if (arr[j] < min) {
-                sortedarr[i] = arr[j];
+    int arr[5] = {21,321,66,32,00};
+    int temp;
+    int n=5;
+    int indexofmix;
+    for(int i=0; i<n-1; i++) {
+        indexofmix = i;
+        for (int j=i+1; j<n; j++) {
+            if (arr[i] > arr[j]) {
+                indexofmix = j;
             }
         }
+        temp = arr[i];
+        arr[i] = arr[indexofmix];
+        arr[indexofmix] = temp;
+    }
+    for (int i=0; i<n; i++) {
+        printf("%d\t", arr[i]);
     }
 }
